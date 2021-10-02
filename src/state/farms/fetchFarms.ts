6cf2d4e667dd63c97f6cd6d6389765efe8b864f6
RiverRocket-finance <90658521+RiverRocket-finance@users.noreply.github.com>
2021-10-02 23:22:22 +0200

    Update fetchFarms.ts

--- src/state/farms/fetchFarms.ts
+++ src/state/farms/fetchFarms.ts
@@ -91,7 +91,7 @@ const fetchFarms = async () => {
         }
       }
 
-      const [info, totalAllocPoint, rivaPerBlock] = await multicall(masterchefABI, [
+      const [info, totalAllocPoint, moonfarmPerBlock] = await multicall(masterchefABI, [
         {
           address: getMasterChefAddress(),
           name: 'poolInfo',
@@ -103,7 +103,7 @@ const fetchFarms = async () => {
         },
         {
           address: getMasterChefAddress(),
-          name: 'rivaPerBlock',
+          name: 'moonfarmPerBlock',
         },
       ])
 
@@ -119,7 +119,7 @@ const fetchFarms = async () => {
         poolWeight: poolWeight.toNumber(),
         multiplier: `${allocPoint.div(100).toString()}X`,
         depositFeeBP: info.depositFeeBP,
-        rivaPerBlock: new BigNumber(rivaPerBlock).toNumber(),
+        moonfarmPerBlock: new BigNumber(moonfarmPerBlock).toNumber(),
       }
     }),
   )
