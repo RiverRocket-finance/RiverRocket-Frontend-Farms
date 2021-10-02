ba3a0291c50cdc7c7a42157c8c6a3927d634e7b2
RiverRocket-finance <90658521+RiverRocket-finance@users.noreply.github.com>
2021-10-02 23:22:26 +0200

    Update types.ts

--- src/state/types.ts
+++ src/state/types.ts
@@ -8,7 +8,7 @@ export interface Farm extends FarmConfig {
   tokenPriceVsQuote?: BigNumber
   poolWeight?: number
   depositFeeBP?: number
-  rivaPerBlock?: number
+  moonfarmPerBlock?: number
     userData?: {
     allowance: BigNumber
     tokenBalance: BigNumber
