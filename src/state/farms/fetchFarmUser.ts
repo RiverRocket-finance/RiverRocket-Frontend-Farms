c508eddd2cdfcf002c7f5c39b98f2d2b1c19cbc6
RiverRocket-finance <90658521+RiverRocket-finance@users.noreply.github.com>
2021-10-02 23:22:18 +0200

    Update fetchFarmUser.ts

--- src/state/farms/fetchFarmUser.ts
+++ src/state/farms/fetchFarmUser.ts
@@ -63,7 +63,7 @@ export const fetchFarmUserEarnings = async (account: string) => {
   const calls = farmsConfig.map((farm) => {
     return {
       address: masterChefAdress,
-      name: 'pendingRiva',
+      name: 'pendingmoonfarm',
       params: [farm.pid, account],
     }
   })
